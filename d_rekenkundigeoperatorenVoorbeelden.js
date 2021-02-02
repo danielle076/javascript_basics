5dd31153540ed02cb8a3ba06859a0388f2d2637e
danielle076 <d.vandenakker@novi-education.nl>
2021-02-02 10:44:20 +0100

    changed rekenkundige operatoren voorbeelden

---
+++ d_rekenkundigeoperatorenVoorbeelden.js
@@ -0,0 +1,38 @@
+// 1. Declareer een variabele en sla het resultaat van 4 plus 5 erin op.
+const add = 4 + 5;
+console.log(add); // geeft 9
+
+// 2. Declareer een variabele en sla het resultaat van 7 maal 52 erin op.
+const multiply = 7 * 52;
+console.log(multiply); // geeft 364
+
+// 3. Declareer een variabele en sla daarin het resultaat van 4 + 3 vermenigvuldigd met 7 op.
+const sum = (4 + 3) * 7;
+console.log(sum); // geeft 49
+
+// 4. Declareer een variabele en sla daarin op de uitkomst van 36 gedeeld door 6, maal 2.
+const secondSum = (36 / 6) * 2;
+console.log(secondSum); // geeft 12
+
+// 5. Declareer twee variabelen en zet daarin respectievelijk de waarden "zoet" en "sappig".
+// Declareer nog een variabele en sla daarin de samenvoeging van de bovenstaande variabelen op.
+const a = 'zoet';
+const b = 'sappig';
+const c = a + b;
+console.log(c); // geeft "zoetsappig"
+
+// 6. Declareer een variabele voor je voornaam en een variabele voor je achternaam en zet de waarden erin.
+// Declareer dan een variabele voor je volledige naam en plak de bovenstaande variabelen aan elkaar.
+// Zorg ervoor dat er een spatie tussen staat zonder iets te veranderen aan de oorspronkelijke variabelen.
+const firstName = 'Daniëlle';
+const lastName = 'van den Akker';
+const fullName = firstName + " " + lastName;
+console.log(fullName); // geeft Daniëlle van den Akker
+
+// 7. Declareer een variabele voor je stad en een variabele voor provincie en zet de waarden erin.
+// Declareer dan een variabele voor je locatie en plak je stad en provincie aan elkaar.
+// Zorg ervoor dat er een komma en spatie tussen staat zonder iets te veranderen aan de oorspronkelijke variabelen.
+const city = 'Breda';
+const state = 'Noord-Brabant';
+const location = city + ", " + state;
+console.log(location); // geeft Breda, Noord-Brabant
--- readme.md
+++ readme.md
@@ -4,14 +4,13 @@ Dit project bevat de volgende bestanden:
 * `a_variabelen.js` + `a_variabelenVoorbeelden.js`
 * `b_console.js`
 * `c_primitievesoorten.js`
-* `d_rekenkundigeoperatoren.js`
+* `d_rekenkundigeoperatoren.js` + `d_rekenkundigeoperatorenVoorbeelden.js`
 * `arrays.js`  
 * `ifstatement.js`
 * `vergelijkingsoperatoren.js`
 * `logischeoperatoren.js`
 * `boolean.js`
 * `loop.js`
-
 
 
 
