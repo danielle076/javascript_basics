7f4412f98ef81010b9b451b5a86f1ef55db95ed7
danielle076 <d.vandenakker@novi-education.nl>
2021-02-06 14:43:54 +0100

    updated gegevens typen

---
+++ c_gegevenstypen.js
@@ -0,0 +1,15 @@
+// DATA TYPES PRIMITIVE
+// Numbers: 4, 223902 en 0,75
+// String: "Hallo!", "Ik ben een string" en "Koekjes zijn lekker"
+// Booleaans: Representeert een logische entiteit en kan twee waarden hebben: waar of onwaar.
+// Null: Dit type heeft slechts één waarde : null.
+// Undefined: Een variabele waaraan geen waarde is toegekend, is undefined.
+
+// DATA TYPE NON-PRIMITIVE
+// Object: Dit is het belangrijkste datatype en vormt de bouwstenen voor het moderne JavaScript.
+
+const numberOfLamps = 4; // number
+const question = "Heb ik het licht wel uitgedaan?"; // string
+const isLightOn = false; // boolean
+let shoppingCart = null; // null
+let car // undefined
--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@
 Dit project bevat de volgende bestanden:
 * `a_variabelen.js` + `a_variabelenVoorbeelden.js`
 * `b_console.js`
-* `c_primitievesoorten.js`
+* `c_gegevenstypen.js`
 * `d_rekenkundigeoperatoren.js` + `d_rekenkundigeoperatorenVoorbeelden.js`
 * `e_arrays.js` + `e_arraysVoorbeelden.js`
 * `f_ifstatement.js`
