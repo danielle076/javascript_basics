a2bd1f004816a1462b7a50cb7ed1130a059f4c32
danielle076 <d.vandenakker@novi-education.nl>
2021-02-06 15:08:37 +0100

    updated variabelen

--- a_variabelen.js
+++ a_variabelen.js
@@ -1,10 +1,26 @@
+// VARIABLES
+// Variabelen zijn containers die herbruikbare gegevens bevatten. Het is de basiseenheid van opslag in een programma.
+// De waarde die in een variabele is opgeslagen, kan tijdens de uitvoering van het programma worden gewijzigd.
+// Een variabele is slechts een naam die aan een geheugenplaats wordt gegeven, alle bewerkingen die op de variabele
+// worden uitgevoerd hebben effect op die geheugenplaats.
+
 // Voordat we variabelen kunnen gebruiken, moeten we eerst aankondigen dat we ze willen gebruiken. Dit wordt gedaan door
 // een variabele aan te maken en deze een naam te geven. Dit heet declareren.
-const quantity;
+let example;
 
-// Door de aanduiding die we gebruiken (const), begrijpt de computer dat we het over een variabele hebben. We kunnen
+// Door de aanduiding die we gebruiken (let), begrijpt de computer dat we het over een variabele hebben. We kunnen
 // er een waarde in opslaan. Dit wordt het toewijzen van een waarde genoemd.
-quantity = 3;
+example = 3;
+
+// We kunnen de variabelen initialiseren op het moment van declaratie of ook later wanneer we ze willen gebruiken. Hieronder
+// staan enkele voorbeelden van het declareren en initialiseren van variabelen in JavaScript.
+
+var name; // declaratie van een enkele variabele
+
+var person, title, num; // meerdere variabelen declareren
+
+var anotherName = "Danielle";
+anotherName = "Freckle"; // overschrijven van variabele Danielle naar Freckle
 
 // Checklist voor variabele namen:
 // De naam begint altijd met een letter, dollarteken ($) of een streepje (_). Hij mag nooit met een getal beginnen.
@@ -21,14 +37,16 @@ quantity = 3;
 // * const: de waarde wordt slechts eenmaal per iteratie toegekend
 // Vóór 2015 was het gebruik van het var keyword de enige manier om een JavaScript variabele te declareren
 
-let greeter = "Hoihoi!";
+// let variable
+let cat = "Freckle";
 
-greeter = "Hallo allemaal!";
+// let kan ook meerdere waarden aangeven
+let a=1,b=2,c=3;
 
-console.log(greeter); // geeft “Hallo allemaal!”
+// let toewijzing
+let d = 3;
+d = 4; // overschrijven van variabele 3 naar 4
 
-const greeter = "Hoihoi!";
-
-greeter = "Hallo allemaal!";
-
-console.log(greeter); // geeft “TypeError: Assignment to constant variable.”
+// const variable
+const hello = "There";
+hello = "You"; // const variabele kun je maar 1 keer toewijzen: TypeError: Assignment to constant variable
