e969bacb56a8d9504fdd1c896db136631d889cdf
danielle076 <d.vandenakker@novi-education.nl>
2021-02-08 16:38:21 +0100

    updated string voorbeelden

--- k_stringVoorbeelden.js
+++ k_stringVoorbeelden.js
@@ -9,3 +9,13 @@ alert("You have written " + tweetCount + " characters, you have " + (140 - tweet
 // Korte versie
 const tweet = prompt("Compose your tweet");
 alert("You have written " + tweet.length + " characters, you have " + (140 - tweet.length) + " characters remaining.")
+
+// 2. Maak een prompt waar de gebruiker een lange string kan invoeren, zoals een paragraaf tekst van een blog post. De
+// gebruiker mag niet meer dan 140 karakters schrijven. Zodra hij aan de 140 karakters zit, snij je de rest van het
+// bericht eraf
+const tweet = prompt("Compose your tweet");
+const tweetUnder140 = tweet.slice(0,140);
+alert(tweetUnder140);
+
+// Korte versie
+alert(prompt("Compose your tweet").slice(0,140));
