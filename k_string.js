04e31c23e6422714948bbf6eb4b1c949624f85a1
danielle076 <d.vandenakker@novi-education.nl>
2021-02-08 15:55:33 +0100

    added string

---
+++ k_string.js
@@ -0,0 +1,25 @@
+// STRING
+// Een string bestaat uit nul of meer tekens die tussen aanhalingstekens staan (niet verwarren met “”).
+
+// Voorbeeld
+let x = "Freckle";
+
+// STRING SAMENGEVOEGD
+// "a" + " " + "b" wordt "a b" samengevoegd tot een enkele string.
+
+// Voorbeeld #1
+console.log("Hello" + "World"); // geeft HelloWorld
+
+// Voorbeeld #2
+// De woorden van voorbeeld #1 zijn aan elkaar geschreven. Je kunt dit op twee manieren oplossen:
+// 1. door " "
+// 2. door een spatie bij één van de woorden
+console.log("Hello" + " " +  "World"); // geeft Hello World
+console.log("Hello" + " World"); // geeft Hello World
+
+// STRING LENGTH
+// Bereken het aantal karakters in een string door de length eigenschap te gebruiken.
+// Wanneer we word.length schrijven, krijg je het aantal tekens in het woord.
+let name = "Freckle";
+console.log(name.length); // geeft 7
+
