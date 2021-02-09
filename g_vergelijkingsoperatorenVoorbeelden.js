55391f3a93ae175e2d73e24aa30c624dd07b738e
danielle076 <d.vandenakker@novi-education.nl>
2021-02-09 09:22:14 +0100

    updated vergelijkingsoperatoren voorbeelden

--- g_vergelijkingsoperatorenVoorbeelden.js
+++ g_vergelijkingsoperatorenVoorbeelden.js
@@ -35,3 +35,26 @@ if (!isRaining && temperature > 21) {
 if (isRaining || temperature < 12) {
     console.log("Put on a coat!");
 }
+
+// 6. Hieronder zie je twee prompts waarbij een naam wordt gevraagd. Na het invoeren van twee namen wordt een percentage
+// tussen de 1 en 100 berekend en teruggeven aan de gebruiker.
+prompt("what is your name?");
+prompt("what is their name?");
+
+let loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+// Schrijf een code waarbij je berichten kan sturen naar de mensen die de volgende scores krijgen:
+// Meer dan 70 geeft bericht "You love each other like Kanye loves Kanye."
+// Tussen de 30 en 70 geeft alleen de score als bericht
+// Onder de 30 "You go together like oil and water."
+if (loveScore > 70){
+    alert("Your love score is " + loveScore + "%" + " You love each other like Kanye loves Kanye.");
+}
+
+if (loveScore > 30 && loveScore <= 70){
+    alert("Your love score is " + loveScore + "%");
+}
+
+if (loveScore <= 30) {
+    alert("Your love score is " + loveScore + "%" + " You go together like oil and water.");
+}
